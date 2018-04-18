857e86502ebe97ba0fcd9710826ea5d514498d0d
Per Vognsen <per.vognsen@gmail.com>
2018-04-18 18:57:58 +0700

    Generate button events via event processing rather than per-frame sampling

--- noir/main.c
+++ noir/main.c
@@ -18,7 +18,7 @@ bool check_init(void) {
     return true;
 }
 
-int noir_to_sdl_scancode[NUM_KEYS] = {
+int noir_key_to_sdl_scancode[NUM_KEYS] = {
     [KEY_RETURN] = SDL_SCANCODE_RETURN,
     [KEY_SPACE] = SDL_SCANCODE_SPACE,
     [KEY_BACKSPACE] = SDL_SCANCODE_BACKSPACE,
@@ -28,38 +28,16 @@ int noir_to_sdl_scancode[NUM_KEYS] = {
     [KEY_RIGHT] = SDL_SCANCODE_RIGHT,
     [KEY_UP] = SDL_SCANCODE_UP,
     [KEY_DOWN] = SDL_SCANCODE_DOWN,
+    [KEY_LSHIFT] = SDL_SCANCODE_LSHIFT,
+    [KEY_RSHIFT] = SDL_SCANCODE_RSHIFT,
+    [KEY_LCTRL] = SDL_SCANCODE_LCTRL,
+    [KEY_RCTRL] = SDL_SCANCODE_RCTRL,
+    [KEY_LALT] = SDL_SCANCODE_LALT,
+    [KEY_RALT] = SDL_SCANCODE_RALT,
+
 };
 
-void update_keys() {
-    const uint8_t *sdl_keys = SDL_GetKeyboardState(NULL);
-    for (int key = 0; key < NUM_KEYS; key++) {
-        int scancode = noir_to_sdl_scancode[key];
-        if (scancode) {
-            update_digital_button(&noir.keys[key], sdl_keys[scancode]);
-        }
-    }
-    struct {
-        int key;
-        uint32_t mask;
-    } sdl_mod_map[] = {
-        {KEY_LSHIFT, KMOD_LSHIFT},
-        {KEY_RSHIFT, KMOD_RSHIFT},
-        {KEY_SHIFT, KMOD_SHIFT},
-        {KEY_LCTRL, KMOD_LCTRL},
-        {KEY_RCTRL, KMOD_RCTRL},
-        {KEY_CTRL, KMOD_CTRL},
-        {KEY_LALT, KMOD_LALT},
-        {KEY_RALT, KMOD_RALT},
-        {KEY_ALT, KMOD_ALT},
-        {KEY_CAPS, KMOD_CAPS},
-    };
-    SDL_Keymod sdl_mod = SDL_GetModState();
-    for (int i = 0; i < sizeof(sdl_mod_map)/sizeof(*sdl_mod_map); i++) {
-        uint32_t mask = sdl_mod_map[i].mask;
-        int key = sdl_mod_map[i].key;
-        update_digital_button(&noir.keys[key], sdl_mod & mask);
-    }
-}
+int sdl_scancode_to_noir_key[SDL_NUM_SCANCODES];
 
 void update_mouse(void) {
     if (noir.mouse.captured != noir.mouse.synced_captured) {
@@ -73,9 +51,6 @@ void update_mouse(void) {
         SDL_WarpMouseInWindow(NULL, noir.mouse.pos.x, noir.mouse.pos.y);
     }
     uint32_t state = SDL_GetMouseState(&noir.mouse.pos.x, &noir.mouse.pos.y);
-    update_digital_button(&noir.mouse.left_button, state & SDL_BUTTON_LEFT);
-    update_digital_button(&noir.mouse.middle_button, state & SDL_BUTTON_MIDDLE);
-    update_digital_button(&noir.mouse.right_button, state & SDL_BUTTON_RIGHT);
     noir.mouse.delta_pos = (int2){noir.mouse.pos.x - noir.mouse.synced_pos.x, noir.mouse.pos.y - noir.mouse.synced_pos.y};
     noir.mouse.moved = noir.mouse.delta_pos.x || noir.mouse.delta_pos.y;
     noir.mouse.synced_pos = noir.mouse.pos;
@@ -90,11 +65,37 @@ void update_mouse(void) {
 }
 
 void update_events(void) {
+    for (int key = 0; key < NUM_KEYS; key++) {
+        reset_digital_button_events(&noir.keys[key]);
+    }
+    reset_digital_button_events(&noir.mouse.left_button);
+    reset_digital_button_events(&noir.mouse.middle_button);
+    reset_digital_button_events(&noir.mouse.right_button);
     SDL_Event event;
     char *text_ptr = noir.text;
     char *text_end = noir.text + sizeof(noir.text) - 1;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
+        case SDL_MOUSEBUTTONDOWN:
+        case SDL_MOUSEBUTTONUP:
+            if (event.button.button == SDL_BUTTON_LEFT) {
+                update_digital_button(&noir.mouse.left_button, event.button.state == SDL_PRESSED);
+            } else if (event.button.button == SDL_BUTTON_MIDDLE) {
+                update_digital_button(&noir.mouse.middle_button, event.button.state == SDL_PRESSED);
+            } else if (event.button.button == SDL_BUTTON_RIGHT) {
+                update_digital_button(&noir.mouse.right_button, event.button.state == SDL_PRESSED);
+            }
+            break;
+        case SDL_KEYDOWN:
+        case SDL_KEYUP:
+            if (!event.key.repeat) {
+                int key = sdl_scancode_to_noir_key[event.key.keysym.scancode];
+                if (key) {
+                    update_digital_button(&noir.keys[key], event.key.state == SDL_PRESSED);
+                    update_combination_keys();
+                }
+            }
+            break;
         case SDL_TEXTINPUT: {
             const char *str = event.text.text;
             while (*str) {
@@ -116,7 +117,6 @@ void update_events(void) {
 
 void update_time(void) {
     uint64 ticks = SDL_GetPerformanceCounter() - noir.time.sdl_start_ticks;
-    noir.time.delta_ticks = ticks - noir.time.ticks;
     noir.time.ticks = ticks;
 
     noir.time.nsecs = (noir.time.ticks * 1000 * 1000 * 1000) / noir.time.ticks_per_sec;
@@ -124,9 +124,10 @@ void update_time(void) {
     noir.time.msecs = (noir.time.ticks * 1000) / noir.time.ticks_per_sec;
     noir.time.secs = (double)noir.time.ticks / (double)noir.time.ticks_per_sec;
 
-    noir.time.delta_nsecs = (noir.time.delta_ticks * 1000 * 1000 * 1000) / noir.time.ticks_per_sec;
-    noir.time.delta_usecs = (noir.time.delta_ticks * 1000 * 1000) / noir.time.ticks_per_sec;
-    noir.time.delta_msecs = (noir.time.delta_ticks * 1000) / noir.time.ticks_per_sec;
+    noir.time.delta_ticks = (int)(ticks - noir.time.ticks);
+    noir.time.delta_nsecs = (int)((noir.time.delta_ticks * 1000 * 1000 * 1000) / noir.time.ticks_per_sec);
+    noir.time.delta_usecs = (int)((noir.time.delta_ticks * 1000 * 1000) / noir.time.ticks_per_sec);
+    noir.time.delta_msecs = (int)((noir.time.delta_ticks * 1000) / noir.time.ticks_per_sec);
     noir.time.delta_secs = (float)noir.time.delta_ticks / (float)noir.time.ticks_per_sec;
 }
 
@@ -174,20 +175,25 @@ bool update(void) {
     update_events();
     update_window();
     update_time();
-    update_keys();
     update_mouse();
     noir.num_updates++;
     return !noir.quit;
 }
 
-void init_scancodes(void) {
+void init_keys(void) {
     for (int c = 0; c < 256; c++) {
         if (isprint(c)) {
             char str[] = {c, 0};
             SDL_Scancode scancode = SDL_GetScancodeFromName(str);
             if (scancode != SDL_SCANCODE_UNKNOWN) {
-                noir_to_sdl_scancode[(unsigned char)c] = scancode;
+                noir_key_to_sdl_scancode[(unsigned char)c] = scancode;
             }
+        }
+    }
+    for (int key = 0; key < NUM_KEYS; key++) {
+        int scancode = noir_key_to_sdl_scancode[key];
+        if (scancode) {
+            sdl_scancode_to_noir_key[scancode] = key;
         }
     }
 }
@@ -234,7 +240,7 @@ bool init(void) {
     if (!init_window()) {
         return false;
     }
-    init_scancodes();
+    init_keys();
     init_time();
     noir.init = true;
     return true;
