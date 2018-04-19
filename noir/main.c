24c7b1669b03d4feb97ce80c246097b665f789ff
Per Vognsen <per.vognsen@gmail.com>
2018-04-19 09:40:14 +0700

    Noir clipboard text support (bidirectional sync)

--- noir/main.c
+++ noir/main.c
@@ -10,22 +10,6 @@
 
 #include "SDL.h"
 
-static bool check_init(void) {
-    if (!app.init) {
-        app.error = "Not initialized";
-        return false;
-    }
-    return true;
-}
-
-static void sdl_error(const char *name) {
-    const char *error = SDL_GetError();
-    if (*error) {
-        sprintf_s(app.error_buf, sizeof(app.error_buf), "%s: %s", name, error);
-        app.error = app.error_buf;
-    }
-}
-
 int noir_key_to_sdl_scancode[NUM_KEYS] = {
     [KEY_RETURN] = SDL_SCANCODE_RETURN,
     [KEY_SPACE] = SDL_SCANCODE_SPACE,
@@ -45,6 +29,92 @@ int noir_key_to_sdl_scancode[NUM_KEYS] = {
 };
 
 int sdl_scancode_to_noir_key[SDL_NUM_SCANCODES];
+
+static void sdl_error(const char *name) {
+    const char *error = SDL_GetError();
+    if (*error) {
+        sprintf_s(app.error_buf, sizeof(app.error_buf), "%s: %s", name, error);
+        app.error = app.error_buf;
+    }
+}
+
+static bool init_display(void) {
+    float dpi;
+    if (SDL_GetDisplayDPI(0, &dpi, NULL, NULL) != 0) {
+        sdl_error("SDL_GetDisplayDPI");
+        return false;
+    }
+    app.display.dpi = dpi;
+
+    SDL_DisplayMode mode;
+    if (SDL_GetCurrentDisplayMode(0, &mode) != 0) {
+        sdl_error("SDL_GetCurrentDisplayMode");
+        return false;
+    }
+    app.display.size.x = mode.w;
+    app.display.size.y = mode.h;
+    app.display.rate = mode.refresh_rate;
+    return true;
+}
+
+static void init_keys(void) {
+    for (int c = 0; c < 256; c++) {
+        if (isprint(c)) {
+            char str[] = {c, 0};
+            SDL_Scancode scancode = SDL_GetScancodeFromName(str);
+            if (scancode != SDL_SCANCODE_UNKNOWN) {
+                noir_key_to_sdl_scancode[(unsigned char)c] = scancode;
+            }
+        }
+    }
+    for (int key = 0; key < NUM_KEYS; key++) {
+        int scancode = noir_key_to_sdl_scancode[key];
+        if (scancode) {
+            sdl_scancode_to_noir_key[scancode] = key;
+        }
+    }
+}
+
+static bool init_window(void) {
+    if (!app.window.title) {
+        app.window.title = default_window_title;
+    }
+    int x = app.window.pos.x == DEFAULT_WINDOW_POS  ? SDL_WINDOWPOS_CENTERED : app.window.pos.x;
+    int y = app.window.pos.y == DEFAULT_WINDOW_POS  ? SDL_WINDOWPOS_CENTERED : app.window.pos.y;
+    int width = app.window.size.x == 0 ? default_window_size.x : app.window.size.x;
+    int height = app.window.size.y == 0 ? default_window_size.y : app.window.size.y;
+    SDL_WindowFlags flags = 0;
+    if (app.window.resizable) {
+        flags |= SDL_WINDOW_RESIZABLE;
+    }
+    if (app.window.hidden) {
+        flags |= SDL_WINDOW_HIDDEN;
+    }
+    SDL_Window *sdl_window = SDL_CreateWindow(app.window.title, x, y, width, height, flags);
+    if (!sdl_window) {
+        app.error = "Window creation failed";
+        return false;
+    }
+    app.window.sdl = sdl_window;
+    app.window.synced_pos = app.window.pos;
+    strcpy_s(app.window.synced_title, sizeof(app.window.synced_title), app.window.title);
+    extern void update_window();
+    update_window();
+    return true;
+}
+
+static void init_time(void) {
+    app.time.ticks_per_sec = SDL_GetPerformanceFrequency();
+    app.time.sdl_start_ticks = SDL_GetPerformanceCounter();
+}
+
+static bool check_init(void) {
+    if (!app.init) {
+        app.error = "Not initialized";
+        return false;
+    }
+    return true;
+}
 
 static void update_mouse(void) {
     if (app.mouse.capture != app.mouse.synced_capture) {
@@ -144,7 +214,7 @@ static void update_time(void) {
 
 static void update_window(void) {
     if (app.window.title != app.window.synced_title && strcmp(app.window.title, app.window.synced_title) != 0) {
-        sdl_error("SDL_SetWindowTitle");
+        SDL_SetWindowTitle(app.window.sdl, app.window.title);
         strcpy_s(app.window.synced_title, sizeof(app.window.synced_title), app.window.title);
         app.window.title = app.window.synced_title;
     }
@@ -178,73 +248,25 @@ static void update_window(void) {
     app.window.synced_hidden = app.window.hidden;
 }
 
-static bool init_display(void) {
-    float dpi;
-    if (SDL_GetDisplayDPI(0, &dpi, NULL, NULL) != 0) {
-        sdl_error("SDL_GetDisplayDPI");
-        return false;
-    }
-    app.display.dpi = dpi;
-
-    SDL_DisplayMode mode;
-    if (SDL_GetCurrentDisplayMode(0, &mode) != 0) {
-        sdl_error("SDL_GetCurrentDisplayMode");
-        return false;
-    }
-    app.display.size.x = mode.w;
-    app.display.size.y = mode.h;
-    app.display.rate = mode.refresh_rate;
-    return true;
-}
-
-static void init_keys(void) {
-    for (int c = 0; c < 256; c++) {
-        if (isprint(c)) {
-            char str[] = {c, 0};
-            SDL_Scancode scancode = SDL_GetScancodeFromName(str);
-            if (scancode != SDL_SCANCODE_UNKNOWN) {
-                noir_key_to_sdl_scancode[(unsigned char)c] = scancode;
+void update_clipboard(void) {
+    // TODO: Concerned about performance implications for large clipboard data.
+    if (app.clipboard != app.synced_clipboard) {
+        SDL_free(app.synced_clipboard);
+        app.synced_clipboard = SDL_strdup(app.clipboard);
+        app.clipboard = app.synced_clipboard;
+        SDL_SetClipboardText(app.clipboard);
+    } else {
+        if (SDL_HasClipboardText()) {
+            char *new_clipboard = SDL_GetClipboardText();
+            if (!app.synced_clipboard || strcmp(new_clipboard, app.synced_clipboard) != 0) {
+                SDL_free(app.synced_clipboard);
+                app.synced_clipboard = new_clipboard;
+                app.clipboard = app.synced_clipboard;
+            } else {
+                SDL_free(new_clipboard);
             }
         }
     }
-    for (int key = 0; key < NUM_KEYS; key++) {
-        int scancode = noir_key_to_sdl_scancode[key];
-        if (scancode) {
-            sdl_scancode_to_noir_key[scancode] = key;
-        }
-    }
-}
-
-static bool init_window(void) {
-    if (!app.window.title) {
-        app.window.title = default_window_title;
-    }
-    int x = app.window.pos.x == DEFAULT_WINDOW_POS  ? SDL_WINDOWPOS_CENTERED : app.window.pos.x;
-    int y = app.window.pos.y == DEFAULT_WINDOW_POS  ? SDL_WINDOWPOS_CENTERED : app.window.pos.y;
-    int width = app.window.size.x == 0 ? default_window_size.x : app.window.size.x;
-    int height = app.window.size.y == 0 ? default_window_size.y : app.window.size.y;
-    SDL_WindowFlags flags = 0;
-    if (app.window.resizable) {
-        flags |= SDL_WINDOW_RESIZABLE;
-    }
-    if (app.window.hidden) {
-        flags |= SDL_WINDOW_HIDDEN;
-    }
-    SDL_Window *sdl_window = SDL_CreateWindow(app.window.title, x, y, width, height, flags);
-    if (!sdl_window) {
-        app.error = "Window creation failed";
-        return false;
-    }
-    app.window.sdl = sdl_window;
-    app.window.synced_pos = app.window.pos;
-    strcpy_s(app.window.synced_title, sizeof(app.window.synced_title), app.window.title);
-    update_window();
-    return true;
-}
-
-static void init_time(void) {
-    app.time.ticks_per_sec = SDL_GetPerformanceFrequency();
-    app.time.sdl_start_ticks = SDL_GetPerformanceCounter();
 }
 
 bool init(void) {
@@ -279,6 +301,7 @@ bool update(void) {
     update_window();
     update_time();
     update_mouse();
+    update_clipboard();
     app.num_updates++;
     return !app.quit;
 }
